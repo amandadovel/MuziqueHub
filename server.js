356197b1908aaffe9147f82255d08ad2f08a062e
TreezCode <TreezCode@users.noreply.github.com>
2019-07-31 11:00:16 -0600

    server && mongo connection / mvc build

---
+++ server.js
@@ -0,0 +1,22 @@
+// Dependencies
+const express = require("express");
+const mongoose = require("mongoose");
+const PORT = process.env.PORT || 3000;
+const app = express();
+
+// Middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Serve up static assets
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static("client/build"));
+}
+
+// Connect to Mongo DB
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/project3db", { useCreateIndex: true, useNewUrlParser: true });
+
+// Start server
+app.listen(PORT, () => {
+    console.log(`🌎 ==> API server now on port ${PORT}!`);
+});
