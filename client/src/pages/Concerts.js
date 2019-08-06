3856b60651c1d739cb225779a1907bfcbf43c3a6
TreezCode <TreezCode@users.noreply.github.com>
2019-08-05 21:03:31 -0600

    api route update

--- client/src/pages/Concerts.js
+++ client/src/pages/Concerts.js
@@ -1,12 +1,20 @@
 import React from "react";
 import Jumbotron from "../components/Jumbotron";
 
-const Concerts = () => (
-    <>  
-        <Jumbotron>
-            <h1>Concerts</h1>
-        </Jumbotron>
-    </>
-);
+class Concerts extends Component {
+    state = {
+
+    };
+
+    render() {
+        return (
+            <>
+                <Jumbotron>
+                    <h1>Concert</h1>
+                </Jumbotron>
+            </>
+        )
+    }
+}
 
 export default Concerts;
