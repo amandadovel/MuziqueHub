3184e0a4a22a2de4bd249ed8ce3f126fb5e8a7ce
TreezCode <TreezCode@users.noreply.github.com>
2019-08-08 00:25:24 -0600

    stash

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,10 +1,10 @@
 const router = require("express").Router();
 const userRoutes = require("./userRoutes");
-const concertRoutes = require("./concertRoutes");
+const artistRoutes = require("./artistRoutes");
 const youtubeRoutes = require("./youtubeRoutes");
 
 router.use("/users", userRoutes);
-router.use("/concerts", concertRoutes);
+router.use("/artist", artistRoutes);
 router.use("/youtube", youtubeRoutes);
 
 module.exports = router;
