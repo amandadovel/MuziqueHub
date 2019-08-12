6447e1ee269332f368ab262ae9c764dbac714729
Amanda Dovel <amandacdovel@gmail.com>
2019-08-12 13:55:34 -0600

    add style update vile update video search page

--- routes/api/artistRoutes.js
+++ routes/api/artistRoutes.js
@@ -2,11 +2,11 @@ const router = require("express").Router();
 const axios = require("axios");
 require('dotenv').config();
 const db = require("../../models/Artist")
+const apiKey = process.env.REACT_APP_AUDIODB_APIKEY;
 
 // Matches with "/api/artist/search"
 router.get("/search", (req, res) => {
         const artistName = req.query.artistName.replace(/\s/g, "+");
-        const apiKey = process.env.REACT_APP_AUDIODB_APIKEY;
 
         axios.get(`https://www.theaudiodb.com/api/v1/json/${apiKey}/search.php?s=${artistName}`)
             .then(results =>
@@ -18,5 +18,27 @@ router.get("/search", (req, res) => {
             .then(filteredResults => res.json(filteredResults))
             .catch(err => res.status(422).json(err));
 });
+
+router.get("/videos", (req, res) => {
+    // const artistName = req.query.artistName.replace(/\s/g, "+");
+    
+    axios.get(`https://www.theaudiodb.com/api/v1/json/${apiKey}/search.php?s=coldplay`)
+    .then(results => results.data.artists
+        .map(artist =>
+            artist.idArtist
+        )
+    )
+    .then(mappedResults => {
+        let artistId = mappedResults[0]; 
+        axios.get(`https://www.theaudiodb.com/api/v1/json/${apiKey}/mvid.php?i=${artistId}`)
+        .then(results => results.data.mvids
+            .map(videos => 
+                videos.strMusicVid)
+            )
+            .then(mappedResults => res.send(mappedResults))
+    })
+    .catch(err => res.status(422).json(err));
+})
+
 
 module.exports = router;
